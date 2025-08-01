8fa993f9f5fbfa0a1b230d003e08d624d0b04dd0
Nicolas Kavalier <priv.nicolas@protonmail.com>
2025-08-01 14:58:59 +0200

    a little refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use core::panic;
 use std::collections::HashMap;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{self, prelude::*};
 use std::path::Path;
 use std::{env, u8, usize};
@@ -9,11 +9,17 @@ fn remove_whitespace(s: &str) -> String{
     s.chars().filter(|c| !c.is_whitespace()).collect()
 }
 
-fn open_file(path: &Path) -> std::io::Result<Vec<u8>>{
-    let mut file = File::open(path)?;
+fn open_file(path: &Path) -> Vec<u8>{
+    let mut file = match File::open(path) {
+        Ok(f) => f,
+        Err(e) => panic!("Could not open {:?}, error: {}", path, e),
+    };
     let mut content: Vec<u8> = Vec::new();
-    file.read_to_end(&mut content)?;
-    Ok(content)
+    match file.read_to_end(&mut content) {
+        Err(e) => panic!("Could not read {:?}, error {}", path, e),
+        _ => (),
+    };
+    content
 }
 
 fn bytes_check(content: Vec<u8>,mut i: usize) -> u8{
@@ -60,14 +66,14 @@ fn get_chars(content: Vec<u8>, length: usize, hash: Vec<u8>) -> Vec<u8>{
     if ret_array[0] == 0 {
         ret_array[0] = bytes_check(content, tmp);
     }
-    return ret_array;
+    ret_array
 }
 
 fn get_password(path: String, length: usize, hash: Vec<u8>) -> String{
-    let content = open_file(Path::new(&path)).unwrap();
+    let content = open_file(Path::new(&path));
     let byte_array = get_chars(content, length, hash);
     let s = String::from_utf8_lossy(&byte_array);
-    return s.to_string();
+    s.to_string()
 }
 
 fn get_length(arg: String) -> usize{
@@ -84,11 +90,11 @@ fn get_saved_path(shortcuts: HashMap<String, String>, keys: Vec<String>) -> Vec<
         for k in keys {
             match shortcuts.get(&k) {
                 Some(v) => paths.push(remove_whitespace(v)),
-                None => println!("{} has no path", k),
+                None => println!("{} has invalid path", k),
             }
         }
     }
-    return paths;
+    paths
 }
 
 fn remove_args(mut args: Vec<String>) -> Vec<String> {
@@ -98,60 +104,62 @@ fn remove_args(mut args: Vec<String>) -> Vec<String> {
     args
 }
 
-/*fn create_config() ->  std::io::Result<()>{
-    println!("eeeee");
-    fs::create_dir()?; /*{
-        Err(e) => {println!("{} failed to create config directory", e); return;},
+fn create_config() ->  bool{
+    match fs::create_dir(home::home_dir().unwrap().join(".config/ripm")) {
+        Err(e) => {println!("{} failed to create config directory", e); return false},
         Ok(_) => (),
-    }*/
-    println!("aaaaaa");
-    let mut f = File::create_new(conf_file)?; /*{
-        Err(e) => {println!("{} failed to create config directory", e); return;},
+    }
+    let conf_file = home::home_dir().unwrap().join(".config/ripm/ripm.conf");
+    let mut f = match File::create_new(conf_file) {
+        Err(e) => {println!("{} failed to create config directory", e); return false},
         Ok(f) => f,
-    };*/
-    println!("bbbbb");
-    f.write_all("default_length: 8\n default_password: password.hash\n".as_bytes())?; /* {
-        Err(e) => println!("{} failed to write into config file", e),
-        _ => ()
-    }*/
-    println!("cccc");
-    Ok(())
-}*/
+    };
+    match f.write_all("default_length: 8\ndefault_password: password.hash\n".as_bytes()) {
+        Err(e) => {println!("{} failed to write into config file", e); return false;},
+        _ => (),
+    }
+    true
+}
 
 fn check_config() -> (usize, Option<String>, HashMap<String, String>){
-    /*if conf_file.exists() == false {
-        println!("{:?}", create_config());
-    } */
     let conf_file = home::home_dir().unwrap().join(".config/ripm/ripm.conf");
-    let content = open_file(&conf_file);
-    let binding = content.unwrap();
-    let tmp = String::from_utf8_lossy(&binding);
-    let lines: Vec<&str> = tmp.split("\n").collect();
+    let mut files = true;
+    if conf_file.exists() == false {
+        files = create_config();
+    }
     let mut length: usize = 8;
-    let mut hash = String::new();
+    let mut hash = None;
     let mut shortcuts: HashMap<String, String> = HashMap::new();
-    for i in 0..lines.len() {
-        if lines[i].len() == 0 {
-            break;
-        }
-        let parts: Vec<&str> = lines[i].split(":").collect();
-        if parts.len() % 2 == 1 {
-            println!("invalid config file");
-            continue;
-        }
-        match parts[0].trim(){
-            "default_length" => length = get_length(parts[1].to_string()),
-            "default_password" => {let passwd_hash = parts[1].trim().to_string(); hash = String::from_utf8_lossy(&open_file(&Path::new(&passwd_hash)).unwrap()).to_string();},
-            _ => {shortcuts.insert(parts[0].to_string(), parts[1].to_string());},
+    if files {
+        let mut password = String::new();
+        let conf_file = home::home_dir().unwrap().join(".config/ripm/ripm.conf");
+        let content = open_file(&conf_file);
+        let binding = content;
+        let tmp = String::from_utf8_lossy(&binding);
+        let lines: Vec<&str> = tmp.split("\n").collect();
+        for i in 0..lines.len() {
+            if lines[i].len() == 0 {
+                break;
+            }
+            let parts: Vec<&str> = lines[i].split(":").collect();
+            if parts.len() % 2 == 1 {
+                println!("invalid config file");
+                continue;
+            }
+            match parts[0].trim(){
+                "default_length" => length = get_length(parts[1].to_string()),
+                "default_password" => {let passwd_hash = parts[1].trim().to_string(); password = String::from_utf8_lossy(&open_file(&Path::new(&passwd_hash))).to_string();},
+                _ => {shortcuts.insert(parts[0].to_string(), parts[1].to_string());},
 
+            }
+        }
+        if password.len() > 0 {
+            let mut pass_vec: Vec<u8> = password.as_bytes().to_vec();
+            pass_vec.remove(pass_vec.len()-1);
+            hash = Some(String::from_utf8_lossy(&pass_vec).to_string());
         }
     }
-    if hash.len() > 0 {
-        let mut hash_vec: Vec<u8> = hash.as_bytes().to_vec();
-        hash_vec.remove(hash_vec.len()-1);
-        hash = String::from_utf8_lossy(&hash_vec).to_string();
-    }
-    return  (length, Some(hash), shortcuts);
+    (length, hash, shortcuts)
 
 }
 
@@ -164,6 +172,14 @@ fn get_options(args: Vec<String>) -> Vec<String> {
         options.push(args[i].clone());
     }
     return options;
+}
+
+fn sanitize_string(s: String) -> Vec<u8> {
+    let mut v = s.as_bytes().to_vec();
+    if v[v.len()-1] == 10 {
+        v.remove(v.len()-1);
+    }
+    v
 }
 
 fn main() {
@@ -182,8 +198,8 @@ fn main() {
         match args[0].as_str() {
             "-l" => length = get_length(args[1].clone()),
             "--length" => length = get_length(args[1].clone()),
-            "-H" => hash = args[1].clone().as_bytes().to_vec(),
-            "--hash" => hash = args[1].clone().as_bytes().to_vec(),
+            "-H" => hash = sanitize_string(args[1].clone()),
+            "--hash" => hash = sanitize_string(args[1].clone()),
             "-p" => paths.append(&mut get_options(args.clone())),
             "--path" => paths.append(&mut get_options(args.clone())),
             "-s" => paths.append(&mut get_saved_path(shortcuts.clone(), get_options(args.clone()))),
@@ -196,13 +212,16 @@ fn main() {
         let mut passwd: String = String::new();
         println!("please enter your password: ");
         io::stdin().read_line(&mut passwd).expect("Failed to get password");
-        hash = passwd.as_bytes().to_vec();
-        hash.remove(hash.len()-1);
+        hash = sanitize_string(passwd);
     }
-    println!("Hash of the password: {:?}",sha256::digest(&hash));
-    if sha256::digest(&hash).to_string() != passwd_hash.unwrap(){
-        println!("Your password may be wrong")
-    } 
+    if passwd_hash != None {
+        if sha256::digest(&hash).to_string() != passwd_hash.unwrap(){
+            println!("Your password may be wrong")
+        } 
+    }
+    else {
+        println!("Hash of the password: {:?}",sha256::digest(&hash));
+    }
     for i in 0..paths.len(){
         let password = get_password(paths[i].clone(), length, hash.clone());
         println!("{}: {}", paths[i], password); 
